e22843fa0f2eb19528379d61ddfefdc70115eb02
astridayala <astriayala06@gmail.com>
2025-04-03 10:20:08 -0600

    commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             BookSeeder::class,
             RoleSeeder::class,
             PermissionSeeder::class,
+            LoanSeeder::class,
         ]);
     }
 }
